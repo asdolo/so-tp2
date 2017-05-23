8b87c32a68a0dfa684be733cc98eaea793be2fe9
Federico aliani <falliani@ws4.labo2>
2017-05-23 15:33:16 -0300

    comentarios nada mas

--- src/ConcurrentHashMap.hpp
+++ src/ConcurrentHashMap.hpp
@@ -18,7 +18,6 @@ public:
 	struct datos_process_file {
 		string filePath;
 		ConcurrentHashMap* c;
-
 	};
 
 	struct maximumStruct {
@@ -41,9 +40,12 @@ public:
 	Lista<pair<string,unsigned int>>* tabla[26];
 
 	ConcurrentHashMap() {
+		//Inicializo el Hash.
 		for (int i = 0; i < 26; i++)
 		{
+			//Creo una lista de pares <Palabra,cantidad de repeticiones> por letra
 			tabla[i]= new Lista<pair<string,unsigned int>>();
+			//Inicializo los mutex de cada lista.
 			pthread_mutex_init(&mutexes[i], NULL);
 		}
 	}
@@ -62,7 +64,8 @@ public:
 		
 		// obtenemos la posicion del array correspondiente a la primer letra del string
 		unsigned int posicion = mapCharToInt(key.at(0));
-		
+
+		//Lockeamos la lista de la primer letra de la palabra
 		pthread_mutex_lock(&mutexes[posicion]);
 		// obtenemos un iterador a la lista que tenemos que modificar dentro del hash
 		Lista<pair<string,unsigned int>>::Iterador iteradorListaActual= tabla[posicion]->CrearIt();
@@ -72,6 +75,7 @@ public:
 			// Hay siguiente, veamos si es la palabra key
 			if (iteradorListaActual.Siguiente().first.compare(key)==0)
 			{
+				//Era la palabra. Entonces salgo del ciclo.
 				loEncontre=true;
 				break;
 			}
@@ -80,7 +84,7 @@ public:
 
 		if (loEncontre)
 		{
-			// incremento la cantidad
+			//Ya existia la palabra,entonces incremento la cantidad
 			iteradorListaActual.Siguiente().second++;
 		}
 		else
@@ -88,17 +92,23 @@ public:
 			// no existe el string, lo agrego con cantidad 1
 			tabla[posicion]->push_front(pair<string, unsigned int>(key,1));
 		}
+		//Desbloqueo el mutex de esa lista.
 		pthread_mutex_unlock(&mutexes[posicion]);
 	}
 
 	bool member(string key) {
 		bool loEncontre=false;
+		// obtenemos la posicion del array correspondiente a la primer letra del string
 		unsigned int posicion= mapCharToInt(key.at(0));
+		//No bloqueamos nada porque deber ser wait-free
+
+		// obtenemos un iterador a la lista que tenemos que buscar dentro del hash
 		Lista<pair<string,unsigned int>>::Iterador iteradorListaActual= tabla[posicion]->CrearIt();
 		while(iteradorListaActual.HaySiguiente()){
 			// Hay siguiente, veamos si es la palabra key
 			if (iteradorListaActual.Siguiente().first.compare(key)==0)
 			{
+				//Es la palabra.Salgo del ciclo
 				loEncontre=true;
 				break;
 			}
@@ -107,25 +117,27 @@ public:
 		return loEncontre;
 	}
 
+	//Esta funcion convierte un char ascii a entero.
 	unsigned int mapCharToInt(char a){
 		return (unsigned int)a-97;
 	}
 	
-
+	//Busca el maximo de la fila ind y lo guarda en el puntero a arreglo de par pasado por parametro
 	void *maximumFila(unsigned int ind, pair<string,unsigned int>* arreglo)
     {
-
 		pair<string,unsigned int> maximo;
+		//Inicializo el maximo en 0 (Las palabras minimo tienen 1 repetición)
 		maximo.second=0;
+		//Lockeo el mutex de cada lista. Aqui hago exclusion mutua con addAndInc
 		pthread_mutex_lock(&mutexes[ind]);
+		// obtenemos un iterador a la lista que tenemos que consultar dentro del hash
 		Lista<pair<string,unsigned int>>::Iterador iteradorListaActual= tabla[ind]->CrearIt();
 
 		while(iteradorListaActual.HaySiguiente()){
-			
+			//Hay siguiente
 			if (iteradorListaActual.Siguiente().second>maximo.second)
 			{
-
-			
+				//Encontre uno mayor	
 				maximo=iteradorListaActual.Siguiente();
 			}
 			iteradorListaActual.Avanzar();
@@ -135,27 +147,35 @@ public:
 		
     }
 
-    static void *thread_func(void* estruc)
+    static void *maximumThread(void* estruc)
     {
+    	//Casteo a la estructura que me pasaron
     	maximumStruct* estructura = (maximumStruct*)estruc;
 
     	unsigned int filaAProcesar;
-    	
+    	//Bloqueo el mutex que tienen todos los threads
+    	//Asi me fijo cual fila esta disponible para procesar
+    	//Y ademas modifico la estructura poniendo como procesada la fila que me ponga a procesar
     	pthread_mutex_lock(&(estructura->mutex));
     	while(estructura->filasDisponibles>0){
-    		
+    		//Alguna fila queda disponible
     		estructura->filasDisponibles--;
+    		//Busco cual es la fila disponible
     		filaAProcesar = estructura->clase->proximaFilaDisponible((estructura->procesadas));
+    		//Desbloqueo el mutex porque ya tengo la fila que tengo que procesar.
     		pthread_mutex_unlock(&(estructura->mutex));
     		
+    		//Busco el maximo de la fila. La funcion maximumFila verifica 
+    		//no estar ejecutandose a la par de addAndInc
     		estructura->clase->maximumFila(filaAProcesar,(estructura->_maximos));
-
+    		//Bloqueo el mutex para consultar si hay alguna fila disponible (El while)
 			pthread_mutex_lock(&(estructura->mutex));
     	}
-    	pthread_mutex_unlock(&(estructura->mutex));
-		
+    	//Desbloqueo. No hay mas filas disponibles
+    	pthread_mutex_unlock(&(estructura->mutex));		
     	
     }
+    //Devuelve la primer fila que este sin procesar y la marca como procesada
 unsigned int proximaFilaDisponible(bool* array){
 	for (unsigned int i = 0; i < 26; i++)
 	{
@@ -170,37 +190,38 @@ unsigned int proximaFilaDisponible(bool* array){
 }
 
 	pair<string, unsigned int> maximum(unsigned int nt){
+		//Creo nt threads
 		pthread_t thread[nt];
+		//Creo una estructura que sera pasada a cada thread
 		maximumStruct estructura;
 		estructura.filasDisponibles=26;
 		estructura.clase = this;
+		//Inicializo el mutex que sera utilizado para asignar filas a los threads
 		pthread_mutex_init(&estructura.mutex, NULL);
 		
-
+		//Inicializo un array de bools de filas procesadas
 		for (int i = 0; i < 26; i++)
 		{
+
 			estructura.procesadas[i]=false;	
 		}
 		
+		//Ejecuto los nt threads
 		for (int i = 0; i < nt; ++i)
 		{
-			pthread_create(&thread[i],NULL,&(ConcurrentHashMap::thread_func),&estructura);
+			pthread_create(&thread[i],NULL,&(ConcurrentHashMap::maximumThread),&estructura);
 		}
 		
+		//Espero que los nt threads terminen
 		for (int i = 0; i < nt; ++i)
 		{
 			pthread_join((thread[i]),NULL);
 		}
 		
-		//Si tengo nt<26  tengo que esperar que terminen los threads y crear nuevos con create?
-		// Espero a que todos terminen de buscar su maximo
-
 		//Comparo los resultados _maximos
 		pair<string,unsigned int> maximoDeLosMaximos=estructura._maximos[0];
-		cout << "Par mas grande indice:" << "0"<< ". Palabra:" <<estructura._maximos[0].first << ". Cantidad:" << estructura._maximos[0].second << "\n";
 		for (int i = 1; i < 26; i++)
 		{
-			cout << "Par mas grande indice:" << i << ". Palabra:" <<estructura._maximos[i].first << ". Cantidad:" << estructura._maximos[i].second << "\n";  
 			if (estructura._maximos[i].second>maximoDeLosMaximos.second)
 			{
 				maximoDeLosMaximos=estructura._maximos[i];
@@ -262,8 +283,8 @@ unsigned int proximaFilaDisponible(bool* array){
 	static void* thread_process_file_aux(void* estructura){
 
 		datos_process_file estr = *((datos_process_file*) (estructura));
-		cout << estr.filePath << "\n";
-		cout << estr.c << "\n";
+		
+		
 
     	estr.c->thread_process_file(estr.filePath, estr.c);
 
