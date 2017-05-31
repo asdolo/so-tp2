6d0fb7f721580cc9d42bae641221d8f7a8426d9b
Asdolo <lraposeiras@dc.uba.ar>
2017-05-31 19:04:55 -0300

    Update ConcurrentHashMap.hpp

--- src/ConcurrentHashMap.hpp
+++ src/ConcurrentHashMap.hpp
@@ -21,7 +21,7 @@ public:
 		ConcurrentHashMap* c;
 	};
 
-	struct maximumStruct {
+	struct maximum_struct {
 		ConcurrentHashMap* clase;
 		unsigned int filasDisponibles;
 		pthread_mutex_t mutex;
@@ -130,111 +130,118 @@ public:
 		return (unsigned int)a-97;
 	}
 	
-	//Busca el maximo de la fila ind y lo guarda en el puntero a arreglo de par pasado por parametro
+	// Busca el máximo de la fila indicada en el parámetro ind y lo guarda en el puntero a arreglo de par del parámetro arreglo
 	void *maximumFila(unsigned int ind, pair<string,unsigned int>* arreglo)
     {
 		pair<string,unsigned int> maximo;
-		//Inicializo el maximo en 0 (Las palabras minimo tienen 1 repetición)
-		maximo.second=0;
-		//Lockeo el mutex de cada lista. Aqui hago exclusion mutua con addAndInc
+		
+		// Inicializo el máximo en 0 (las palabras que estén como mínimo tienen 1 repetición)
+		maximo.second = 0;
+		// Bloqueo el mutex de la lista que voy a revisar. Aquí hago exclusión mutua con addAndInc
 		pthread_mutex_lock(&mutexes[ind]);
-		// obtenemos un iterador a la lista que tenemos que consultar dentro del hash
-		Lista<pair<string,unsigned int>>::Iterador iteradorListaActual= tabla[ind]->CrearIt();
+		// obtenemos un iterador a la lista que tenemos que revisar
+		Lista<pair<string, unsigned int>>::Iterador iteradorListaActual = tabla[ind]->CrearIt();
 
 		while(iteradorListaActual.HaySiguiente()){
-			//Hay siguiente
-			if (iteradorListaActual.Siguiente().second>maximo.second)
+			// Hay siguiente
+			if (iteradorListaActual.Siguiente().second > maximo.second)
 			{
-				//Encontre uno mayor	
-				maximo=iteradorListaActual.Siguiente();
+				// Encontré uno mayor
+				maximo = iteradorListaActual.Siguiente();
 			}
 			iteradorListaActual.Avanzar();
 		}
-		arreglo[ind]=maximo;
-		pthread_mutex_unlock(&mutexes[ind]);
-		
+		arreglo[ind] = maximo;
+		pthread_mutex_unlock(&mutexes[ind]);		
     }
 
-    static void *maximumThread(void* estruc)
+    static void *maximumThread(void* data)
     {
-    	//Casteo a la estructura que me pasaron
-    	maximumStruct* estructura = (maximumStruct*)estruc;
+    	// Casteo lo que me pasaron al tipo (maximum_struct*)
+    	maximum_struct* estructura = (maximum_struct*) data;
 
     	unsigned int filaAProcesar;
-    	//Bloqueo el mutex que tienen todos los threads
-    	//Asi me fijo cual fila esta disponible para procesar
-    	//Y ademas modifico la estructura poniendo como procesada la fila que me ponga a procesar
+    	// Bloqueo el mutex que tienen todos los threads
+    	// así me fijo qué fila está disponible para procesar
+    	// y además modifico la estructura poniendo como procesada la fila que me ponga a procesar
     	pthread_mutex_lock(&(estructura->mutex));
     	while(estructura->filasDisponibles>0){
-    		//Alguna fila queda disponible
+    		// Alguna fila queda disponible
     		estructura->filasDisponibles--;
-    		//Busco cual es la fila disponible
+    		// Busco cuál es dicha fila disponible
     		filaAProcesar = estructura->clase->proximaFilaDisponible((estructura->procesadas));
-    		//Desbloqueo el mutex porque ya tengo la fila que tengo que procesar.
+    		// Desbloqueo el mutex porque ya tengo la fila que tengo que procesar y ya la marqué como procesada,
+		// así que ningún otro thread me va a molestar con esta fila
     		pthread_mutex_unlock(&(estructura->mutex));
     		
-    		//Busco el maximo de la fila. La funcion maximumFila verifica 
-    		//no estar ejecutandose a la par de addAndInc
-    		estructura->clase->maximumFila(filaAProcesar,(estructura->_maximos));
-    		//Bloqueo el mutex para consultar si hay alguna fila disponible (El while)
-			pthread_mutex_lock(&(estructura->mutex));
+    		// Busco el máximo de la fila. La función maximumFila verifica 
+    		// no estar ejecutándose a la par de addAndInc
+    		estructura->clase->maximumFila(filaAProcesar, (estructura->_maximos));
+    		// Bloqueo el mutex para consultar si hay alguna fila disponible (El while)
+		pthread_mutex_lock(&(estructura->mutex));
     	}
-    	//Desbloqueo. No hay mas filas disponibles
+    	// Desbloqueo. No hay mas filas disponibles
     	pthread_mutex_unlock(&(estructura->mutex));		
     	
     }
-    //Devuelve la primer fila que este sin procesar y la marca como procesada
-unsigned int proximaFilaDisponible(bool* array){
+	
+    
+    // Recibe como parámetro un arreglo de tamaño 26 de bools, los cuales indican si
+    // la lista correspondiente a la posición i-ésima del arreglo ya fué procesada por algún thread
+    // en la función maximumThread
+    // Devuelve el índide de la primer fila que esté sin procesar (el primer bool en false) y la marca como procesada (true)
+    unsigned int proximaFilaDisponible(bool* array){
 	for (unsigned int i = 0; i < 26; i++)
 	{
-		if (array[i]==false)
+		if (array[i] == false)
 		{
-			array[i]=true;
+			array[i] = true;
 			return i;
 		}			
 	}
-	//Nunca se deberia ejecutar.
+	    
+	// Nunca se deberia ejecutar.
 	return 0;
-}
+    }
 
 	pair<string, unsigned int> maximum(unsigned int nt){
-		//Creo nt threads
+		// Creo nt threads
 		pthread_t thread[nt];
-		//Creo una estructura que sera pasada a cada thread
-		maximumStruct estructura;
-		estructura.filasDisponibles=26;
+		// Creo una estructura que será pasada a cada thread
+		maximum_struct estructura;
+		estructura.filasDisponibles = 26;
 		estructura.clase = this;
-		//Inicializo el mutex que sera utilizado para asignar filas a los threads
+		// Inicializo el mutex que será utilizado para asignar filas a los threads
 		pthread_mutex_init(&estructura.mutex, NULL);
 		
-		//Inicializo un array de bools de filas procesadas
+		// Inicializo un array de bools de filas procesadas
 		for (int i = 0; i < 26; i++)
 		{
-
-			estructura.procesadas[i]=false;	
+			estructura.procesadas[i] = false;	
 		}
 		
 		//Ejecuto los nt threads
 		for (int i = 0; i < nt; ++i)
 		{
-			pthread_create(&thread[i],NULL,&(ConcurrentHashMap::maximumThread),&estructura);
+			pthread_create(&thread[i], NULL, &(ConcurrentHashMap::maximumThread), &estructura);
 		}
 		
-		//Espero que los nt threads terminen
+		// Espero que los nt threads terminen
 		for (int i = 0; i < nt; ++i)
 		{
-			pthread_join((thread[i]),NULL);
+			pthread_join(thread[i], NULL);
 		}
 		
-		//Comparo los resultados _maximos
-		pair<string,unsigned int> maximoDeLosMaximos=estructura._maximos[0];
+		// Comparo los resultados en estructura._maximos
+		pair<string, unsigned int> maximoDeLosMaximos = estructura._maximos[0];
 		for (int i = 1; i < 26; i++)
 		{
-			if (estructura._maximos[i].second>maximoDeLosMaximos.second)
+			if (estructura._maximos[i].second > maximoDeLosMaximos.second)
 			{
-				maximoDeLosMaximos=estructura._maximos[i];
+				maximoDeLosMaximos = estructura._maximos[i];
 			}
 		}
+		
 		return maximoDeLosMaximos;
 	}
 
